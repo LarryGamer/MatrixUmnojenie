d193384ec02a1082b57920b0ad40d5c4b7766003
Alexey <kozaa.19@uni-dubna.ru>
2020-10-06 21:29:20 +0300

    Изменил конверты

--- WindowsFormsApp4/Form1.cs
+++ WindowsFormsApp4/Form1.cs
@@ -18,21 +18,21 @@ namespace WindowsFormsApp4
             
             InitializeComponent();
             //Привязка первой
-            dataGridView1.RowCount =Convert.ToInt32(tbColumns1);
-            dataGridView1.ColumnCount = Convert.ToInt32(tbRows1);
+            dataGridView1.RowCount = int.Parse(tbColumns1.Text);
+            dataGridView1.ColumnCount = int.Parse(tbRows1.Text);
             int i = 0;
             int j = 0;
             int[,] mas1 = new int[i, j];
-            for (i = 0; i < Convert.ToInt32(tbColumns1); ++i)
-                for (j = 0; j < Convert.ToInt32(tbRows1); ++j)
+            for (i = 0; i < int.Parse(tbColumns1.Text); ++i)
+                for (j = 0; j < int.Parse(tbRows1.Text); ++j)
                     dataGridView1.Rows[i].Cells[j].Value = mas1[i, j];
 
             //Привязка второй
-            dataGridView2.RowCount = Convert.ToInt32(tbColumns2);
-            dataGridView2.ColumnCount = Convert.ToInt32(tbRows2);
+            dataGridView2.RowCount = int.Parse(tbColumns2.Text);
+            dataGridView2.ColumnCount = int.Parse(tbRows2.Text);
             int[,] mas2 = new int[i, j];
-            for (i = 0; i < Convert.ToInt32(tbColumns1); ++i)
-                for (j = 0; j < Convert.ToInt32(tbRows1); ++j)
+            for (i = 0; i < int.Parse(tbColumns1.Text); ++i)
+                for (j = 0; j < int.Parse(tbRows1.Text); ++j)
                     dataGridView1.Rows[i].Cells[j].Value = mas2[i, j];
         }
 
@@ -41,8 +41,8 @@ namespace WindowsFormsApp4
 
         private void generatorMatrix_Click(object sender, EventArgs e)
         {
-            createMatrix(Convert.ToInt32(tbRows1.Text), Convert.ToInt32(tbColumns1.Text), M1);
-            createMatrix(Convert.ToInt32(tbRows2.Text), Convert.ToInt32(tbColumns2.Text), M2);
+            createMatrix(int.Parse(tbRows1.Text), int.Parse(tbColumns1.Text), M1);
+            createMatrix(int.Parse(tbRows2.Text), int.Parse(tbColumns2.Text), M2);
             //Вызов функции для  создания 
         }
 
